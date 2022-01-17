d8f5de1684b992118cc3807a80f7cf8641b764be
SeanCho1996 <seancho1996@github.com>
2022-01-17 17:07:30 +0800

    update code and docs

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ultramarine_meme
 a simple YOASOBI &lt;Ultramarine> meme pic generator
+
+## How to use
+First download this git to your own directory:
+```shell
+git clone https://github.com/SeanCho1996/ultramarine_meme.git
+```
+Make sure you have installed all the dependencies:
+```shell
+pip install -r requirements.txt
+```
+Then run with
+```shell
+python ultramarine.py -i PATH_TO_YOUR_IMAGE
+```
+## Potential Issues
+You may need to modifie Line 87:
+```python
+font_path = f"/System/Library/Fonts/STHeiti Light.ttc"
+```
+to your own font path if you're not using a MacOS device.
--- ultramarine.py
+++ ultramarine.py
@@ -8,17 +8,20 @@ from matplotlib import pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", type=str, default="./Abyssinian_20.jpg")
 
-args = parser.parse_args(args=[])
+# args = parser.parse_args(args=[])
+args = parser.parse_args()
+
 img_path = args.image
 
 img = Image.open(img_path)
 img_arr = np.array(img)
+plt.imshow(img)
 
 # %%
 um = np.zeros_like(img_arr)
 um[:, :, 0] = 78
 um[:, :, 1] = 114
-um[:, :, 2] = 190
+um[:, :, 2] = 184
 
 add = cv2.addWeighted(img_arr, 0.3, um, 0.7, 0)
 # plt.imshow(add)
@@ -41,12 +44,12 @@ add = cv2.addWeighted(img_arr, 0.3, um, 0.7, 0)
 # %%
 shorter_side = min(img.size)
 img_gs = cv2.cvtColor(img_arr, cv2.COLOR_RGB2GRAY)
-k_size = int(np.ceil(shorter_side / 400) * 6)
+k_size = int(shorter_side / 400 * 6)
 img_gs = cv2.blur(img_gs, (k_size, k_size))
-grad_x = cv2.Sobel(img_gs, -1, 1, 0, ksize=3)  
-grad_y = cv2.Sobel(img_gs, -1, 0, 1, ksize=3)  
-
-border = cv2.addWeighted(grad_x, 0.5, grad_y, 0.5, 0)
+plt.imshow(img_gs, cmap="gray")
+# %%
+k_size= 5
+border = cv2.Canny(img_gs, 10, 15, k_size)
 # border = cv2.adaptiveThreshold(border, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 301, 0)
 _, border = cv2.threshold(border, 0, 255, cv2.THRESH_OTSU)
 # plt.imshow(border, cmap='gray')  
@@ -63,7 +66,7 @@ for i in range(int(textSize / 2), img.size[0] - rec_length - int(textSize / 2), 
     space = border[j : j + rec_height, i :i + rec_length]
     # print(np.max(space))
     if np.max(space) == 0:
-      # draw.rectangle(((i, j), (i + rec_length, j + rec_height)), fill=None, outline='red', width = 5)
+      draw.rectangle(((i, j), (i + rec_length, j + rec_height)), fill=None, outline='red', width = 5)
       potential_y.append(j)
       potential_x.append(i)
 
@@ -77,19 +80,21 @@ draw = ImageDraw.Draw(img_text)
 if len(potential_x) == 0:
   left = textSize / 2
   top = textSize / 2
+else:
+  left = potential_x[len(potential_x) // 2]
+  top = potential_y[len(potential_x) // 2]
 
-left = potential_x[len(potential_x) // 2]
-top = potential_y[len(potential_x) // 2]
+font_path = f"/System/Library/Fonts/STHeiti Light.ttc"
 fontStyle1 = ImageFont.truetype(
-  "./Heiti.ttc", textSize, encoding="utf-8")
+  font_path, textSize, encoding="utf-8")
 fontStyle2 = ImageFont.truetype(
-  "./Heiti.ttc", int(textSize * 2 / 7), encoding="utf-8")
+  font_path, int(textSize * 2 / 7), encoding="utf-8")
 
 textColor = (255, 255, 255)
 draw.text((left, top), '群青', textColor, font=fontStyle1)
 draw.text((left + int(textSize / 7), top + textSize + int(textSize / 6)), 'YOASOBI', textColor, font=fontStyle2)
 # draw.rectangle(((left, top), (left + 2 * textSize, top + textSize)), fill=None, outline='red', width = 5)
-plt.imshow(img_text)
+# plt.imshow(img_text)
 img_text.save("./ultramarine.png")
 
 
